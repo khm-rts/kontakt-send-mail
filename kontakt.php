e6e18ce7f0ac347b5350219f44c03e5ecad9ac64
khm-rts <khm@rts.dk>
2016-10-02 17:35:46 +0200

    Rettet lidt syntax

--- kontakt.php
+++ kontakt.php
@@ -18,7 +18,7 @@
     // Hvis form er sendt køres dette kode
     if ( isset($_POST['kontakt']) )
     {
-		// Overskriver værdier til kontaktformular, så hvis validering fejler, gemmes input
+    	// Overskriver værdier til kontaktformular, så hvis validering fejler, gemmes input
         $kontakt_navn	= $_POST['navn'];
         $kontakt_email	= $_POST['email'];
         $kontakt_emne	= $_POST['emne'];
@@ -27,15 +27,15 @@
         // Hvis en af felterne er tomme udskrives fejl
         if ( empty($kontakt_navn) || empty($kontakt_email) || empty($kontakt_emne)  || empty($kontakt_besked) )
         {
-            echo '<p class="text-danger">Fejl! Ikke alle felter er udfyldt</p>';
+            echo '<p>Fejl! Ikke alle felter er udfyldt</p>';
         }
         // Ellers kan vi sende e-mail
         else
         {
-            $modtager	= 'modtager@email.dk';
+            $modtager	 = 'modtager@email.dk';
             // For at sende HTML e-mail, skal bl.a. Content-Type og charset defineres i header
-            $headers  = 'MIME-Version: 1.0' . "\r\n";
-            $headers .= "Content-Type: text/html; charset=UTF-8 \r\n";
+            $headers	 = "MIME-Version: 1.0 \r\n";
+            $headers	.= "Content-Type: text/html; charset=UTF-8 \r\n";
             
             // Yderligere headers, hvor bl.a. afsender af e-mail angives
             //$headers	.= "To: Mary <mary@example.com>, Kelly <kelly@example.com>\r\n";
@@ -45,9 +45,9 @@
             
             // Send mail vha. funktionen mail() og hvis det lykkedes vises besked
             if ( mail($modtager, $kontakt_emne, $kontakt_besked, $headers) )
-            {				
-				// Vis besked til bruger om at e-mailen er afsendt
-				echo '<p>Tak for din henvendelse. Vi vil svare hurtigst muligt!</p>';
+            {
+            	// Vis besked til bruger om at e-mailen er afsendt
+				//echo '<p>Tak for din henvendelse. Vi vil svare hurtigst muligt!</p>';
 			}
 			// Hvis ikke det lykkedes at sende e-mail, vises fejlbesked
 			else
@@ -65,23 +65,23 @@
 			<input type="text" name="navn" id="navn" placeholder="For- og efternavn" required autofocus value="<?php echo $kontakt_navn ?>">
 		</p>
 
-        <p>
+		<p>
 			<label for="email" >E-mailadresse:</label><br>
 			<input type="email" name="email" id="email" placeholder="example@domain.com" required value="<?php echo $kontakt_email ?>">
 		</p>
 
-        <p>
+		<p>
 			<label for="emne">Emne:</label><br>
 			<input type="text" name="emne" id="emne" required value="<?php echo $kontakt_emne ?>">
 		</p>
 
-        <p>
+		<p>
 			<label for="kontakt_besked">Besked:</label><br>
 			<textarea rows="4" name="besked" id="kontakt_besked" required><?php echo $kontakt_besked ?></textarea>
 		</p>
-        
-        <button type="submit" name="kontakt">Send</button>
+
+		<button type="submit" name="kontakt">Send</button>
 		<button type="reset">Nulstil</button>
-    </form>
+	</form>
 </body>
 </html>
